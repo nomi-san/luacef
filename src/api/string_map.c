0115e078946607591e7c9a5315f1d27b1df28303
wy3 <nhd.may.21@gmail.com>
2018-12-06 20:34:05 +0700

    add CefStringMap

---
+++ src/api/string_map.c
@@ -0,0 +1,53 @@
+#include "../luacef.h"
+#include "include/internal/cef_string_map.h"
+
+static const char __string_map__[] = "CefStringMap";
+
+int luacef_StringMap_new(lua_State *L)
+{
+	cef_string_map_t map = cef_string_map_alloc();
+
+	cef_string_map_alloc;
+
+	return 1;
+}
+
+
+
+int luacef_StringMap_index(lua_State *L)
+{
+	cef_string_map_t map = luacef_toudata(L, 1);
+
+	lua_pushlightuserdata(L, map);
+	return 1;
+}
+
+int luacef_StringMap_unm(lua_State *L)
+{
+	cef_string_map_t map = luacef_toudata(L, 1);
+
+	lua_pushlightuserdata(L, map);
+	return 1;
+}
+
+
+
+
+// ==================================================
+
+static const luaL_Reg luacef_StringMap_meta[] = {
+	{ "__index", luacef_StringMap_index },
+	{ "__call", luacef_StringMap_index },
+	{ "__unm", luacef_StringMap_unm },
+	{ NULL, NULL }
+};
+
+void luacef_StringMap_reg(lua_State *L)
+{
+	luaL_newmetatable(L, __string_map__);
+	luaL_setfuncs(L, luacef_StringMap_meta, 0);
+	lua_pop(L, 1);
+
+	lua_pushcfunction(L, luacef_StringMap_new);
+	lua_setfield(L, -2, "newStringMap");
+}
