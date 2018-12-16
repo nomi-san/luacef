5899a549d0f8f755c029d4d08168f834c538256e
Duy .Ng <38210249+wy3@users.noreply.github.com>
2018-12-16 14:56:08 +0700

    update

--- src/api/ssl_info.c
+++ src/api/ssl_info.c
@@ -1,67 +1,104 @@
 #include "../luacef.h"
+#include "include/capi/cef_ssl_info_capi.h"
 
-#include "include/capi/cef_values_capi.h"
-#include "include/capi/cef_x509_certificate_capi.h"
+#define API(fn) \
+	LCEF_API(FindHandler, fn)
 
-typedef struct luacef_sslinfo {
-	///
-	// Base structure.
-	///
-	cef_base_ref_counted_t base;
+#define API_N(fn) \
+	LCEF_API_N(FindHandler, fn)
 
-	///
-	// Returns a bitmask containing any and all problems verifying the server
-	// certificate.
-	///
-	cef_cert_status_t(CEF_CALLBACK* get_cert_status)(struct _cef_sslinfo_t* self);
+#define API_M(mname) \
+	LCEF_M(FindHandler, mname)
 
-	///
-	// Returns the X.509 certificate.
-	///
-	struct _cef_x509certificate_t*(CEF_CALLBACK* get_x509certificate)(
-		struct _cef_sslinfo_t* self);
-} luacef_sslinfo;
+#define SELF luacef_SSLInfo
 
-//#include "include/cef_ssl_info.h"
+typedef cef_sslinfo_t  SELF;
 
 
+API(GetCertStatus)
+{
+	SELF *p = luacef_toudata(L, 1);
 
+	cef_cert_status_t ret = p->get_cert_status(p);
 
+	lua_pushinteger(L, ret);
+	return 1;
+}
 
-// static function ==========================================
+API(GetX509Certificate)
+{
+	SELF *p = luacef_toudata(L, 1);
+
+	cef_x509certificate_t *ret = p->get_x509certificate(p);
+
+	luacef_pushudata(L, ret, __x509certificate__);
+	return 1;
+}
+
+API(unm)
+{
+	SELF *p = luacef_toudata(L, 1);
+
+	lua_pushlightuserdata(L, p);
+	return 1;
+}
+
+API(len)
+{
+	lua_pushinteger(L, sizeof(SELF));
+	return 1;
+}
+
+API_M(meta)
+{
+	{ "GetCertStatus", API_N(GetCertStatus) },
+	{ "GetX509Certificate", API_N(GetX509Certificate) },
+
+	{ "__len", API_N(len) },
+	{ "__unm", API_N(unm) },
+
+	LUAREGEND
+};
+
+// static functions ==========================================
 
 /*
-	<bool> IsCertStatusError(
+	<int> IsCertStatusError(
 		<int> status
 	)
 */
 static int luacef_IsCertStatusError(lua_State* L)
 {
-	cef_cert_status_t stt = luaL_checkinteger(L, 1);
+	cef_cert_status_t stt = lua_tointeger(L, 1);
 
-	lua_pushboolean(L, cef_is_cert_status_error(stt));
+	int ret = cef_is_cert_status_error(stt);
+
+	lua_pushinteger(L, ret);
 	return 1;
 }
 
 /*
-	<bool> IsCertStatusMinorError(
+	<int> IsCertStatusMinorError(
 		<int> status
 	)
 */
 static int luacef_IsCertStatusMinorError(lua_State* L)
 {
-	cef_cert_status_t stt = luaL_checkinteger(L, 1);
+	cef_cert_status_t stt = lua_tointeger(L, 1);
 
-	lua_pushboolean(L, cef_is_cert_status_minor_error(stt));
+	int ret = cef_is_cert_status_minor_error(stt);
+
+	lua_pushinteger(L, ret);
 	return 1;
 }
 
 // ==================================================
 
-// ==================================================
-
-void luacef_SSLInfo(lua_State *L)
+void luacef_SSLInfo_reg(lua_State *L)
 {
+	luaL_newmetatable(L, "CefSSLInfo");
+	luaL_setfuncs(L, API_N(meta), 0);
+	lua_setfield(L, -1, __index__);
 
 	lua_pushcfunction(L, luacef_IsCertStatusError);
 	lua_setfield(L, -2, "IsCertStatusError");
